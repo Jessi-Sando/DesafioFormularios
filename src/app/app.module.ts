3f9fbc629530e8b898c6721ab4e341d45b506269
Jessi Sandoval <jessicaandreasandoval@gmail.com>
2023-04-04 20:39:31 -0300

    primer commit del desafio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsComponent } from './formularios/reactive-forms/reactive-forms.component';
+import { TemplateDrivenFormsComponent } from './formularios/template-driven-forms/template-driven-forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormsComponent,
+    TemplateDrivenFormsComponent
   ],
   imports: [
     BrowserModule
